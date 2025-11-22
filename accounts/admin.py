7bf5f33cae27547502c9b0e48148f6a977f5e7c2
Kiryl Alishkevich <kirill.olishkevitch@gmail.com>
2025-11-22 17:09:53 +0100

    Fixed some fields names in accounts/admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -7,11 +7,11 @@ from .models import CustomUser, Roles
 # ------------------------
 class CustomUserAdmin(UserAdmin):
     model = CustomUser
-    list_display = ('email', 'first_name', 'last_name', 'get_mentor', 'role_id', 'is_staff', 'is_active', 'status')
-    list_filter = ('is_staff', 'is_active', 'status', 'role_id')
+    list_display = ('email', 'first_name', 'last_name', 'get_mentor', 'role', 'is_staff', 'is_active', 'status')
+    list_filter = ('is_staff', 'is_active', 'status', 'role')
     
     fieldsets = (
-        (None, {'fields': ('email', 'password', 'role_id', 'mentor_id')}),
+        (None, {'fields': ('email', 'password', 'role', 'mentor')}),
         ('Personal Info', {'fields': ('first_name', 'last_name')}),
         ('Permissions', {'fields': ('is_staff', 'is_active', 'is_superuser', 'groups', 'user_permissions')}),
         ('Important dates', {'fields': ('last_login', 'created_at')}),
@@ -20,7 +20,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('email', 'first_name', 'last_name', 'role_id', 'mentor_id', 'password1', 'password2', 'is_staff', 'is_active')}
+            'fields': ('email', 'first_name', 'last_name', 'role', 'mentor', 'password1', 'password2', 'is_staff', 'is_active')}
         ),
     )
     
@@ -29,7 +29,7 @@ class CustomUserAdmin(UserAdmin):
 
     # Funkcja wyświetlająca mentora w list_display
     def get_mentor(self, obj):
-        return obj.mentor_id  # Wyświetla powiązanego użytkownika
+        return obj.mentor  # Wyświetla powiązanego użytkownika
     get_mentor.short_description = 'Mentor'
 
 # ------------------------
@@ -38,7 +38,7 @@ class CustomUserAdmin(UserAdmin):
 class RolesAdmin(admin.ModelAdmin):
     list_display = ('role_id', 'name', 'description')
     search_fields = ('name',)
-    ordering = ('role_id',)
+    ordering = ('role_id', 'name')
 
 # ------------------------
 # Rejestracja modeli
