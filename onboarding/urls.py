4491652d7e71daf3362190970e1cd983299a2d8d
JaroslawKlepadlo <jarek.klepadlo@gmail.com>
2025-12-10 16:48:01 +0100

    Added a basic competency paths View

--- onboarding/urls.py
+++ onboarding/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import user_tasks_list
+from .views import user_tasks_list, user_competency_paths_with_tasks
 
 app_name = "onboarding"
 
 urlpatterns = [
-    path('onboarding/', user_tasks_list, name='user_tasks_list'),
+    path('onboarding/tasks/', user_tasks_list, name='user_tasks_list'),
+    path('onboarding/paths/', user_competency_paths_with_tasks, name='user_competency_paths_with_tasks'),
 ]
