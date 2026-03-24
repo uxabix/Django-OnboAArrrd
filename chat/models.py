243925285062c23f7909906a093275f81dd53550
JaroslawKlepadlo <jarek.klepadlo@gmail.com>
2026-03-25 00:53:17 +0100

    Changed foreign key field type for Messages model

--- chat/models.py
+++ chat/models.py
@@ -1,18 +1,18 @@
 from django.db import models
 from django.utils import timezone
-from django.contrib.auth import get_user_model
 
-CustomUser = get_user_model()
+from OnboAArrrd import settings
+
 # Create your models here.
 class Messages(models.Model):
     message_id = models.BigAutoField(primary_key=True)
     sender = models.ForeignKey(
-        CustomUser,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="sent_messages"
     )
     receiver = models.ForeignKey(
-        CustomUser,
+        settings.AUTH_USER_MODEL,
         on_delete=models.CASCADE,
         related_name="received_messages"
     )
